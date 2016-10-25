fc62438ebf78bc0222917d0aea073c192737a349
georgecode <georgeczurlanis@gmail.com>
2016-10-25 00:59:47 -0700

    Add interview questions and store answers

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+puts("What is your name??")
+name = gets.chomp
+
+puts("How old are you?")
+age = gets.chomp
+
+puts("What year were you born?")
+year = gets.chomp
+
+puts("Our company cafeteria serves garlic bread. Should we order some for you? ")
+garlic_bread = gets.chomp
+
+puts("Would you like to enroll in the company’s health insurance?")
+insurance = gets.chomp
+
+puts("Glad to meet you #{name}.\nYour age #{age} years old.\nYou were born in #{year}.\nYou said #{garlic_bread} to garlic bread.\nYou said #{insurance} to insurance.\nHave a good day!")
