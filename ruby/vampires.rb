2359da061a123ebd18c4d9e9bd2632589bc25d19
georgecode <georgeczurlanis@gmail.com>
2016-10-25 20:53:50 -0700

    Add extra code to meet criteria

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -41,7 +41,9 @@ for i in 1..num_employees
   end
 
   puts "Name any allergies you have, type done when finished"
+
   allergies = nil
+
   while allergies != "done"
     allergies = gets.chomp
     if allergies == "sunshine"
@@ -50,29 +52,42 @@ for i in 1..num_employees
     end
   end
 
-  puts responce
-
   puts"Employee num #{i} is #{responce}"
 end
 
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
 
+#Below is just to meet challange requirements needed to demonstrate use of elsif, else, <=, >,
+#Release 2 stated "Your program should base its result on the latest condition matched, not the first condition matched. In other words, it's not a "only one condition will apply" sort of scenario." so I used multiable if statements instead if if else
+requirements = "---Below is just to meet challange requirements----"
+puts requirements
+puts requirements
+puts requirements
+
+puts "Do you prefer dogs cats or another type of pet"
+pet = gets.chomp
 
 
-# responce = "no vamp"
-# puts "Name any allergies you have, type done when finished"
-# allergies = nil
-# while allergies != "done"
-#   allergies = gets.chomp
-#   if allergies == "sunshine"
-#     responce ="Definitely a vampire."
-#     allergies = "done"
-#   end
-# end
-# puts responce
+if pet == "dogs"
+  puts "Dogs are usualy super playful, but sometimes a handful"
+elsif pet == "cats"
+  puts "Cats are usualy easy, but sometimes not very playful"
+else
+  puts "Wow thats an intresting choice"
+end
 
-
+puts "How many #{pet} is ideal??"
+num_pets = gets.chomp
+num_pets = num_pets.to_i
+if num_pets <=3
+  puts "Sounds like a good number"
+elsif num_pets > 3 && num_pets <= 6
+  puts "Wow you're a real #{pet} person"
+else
+  puts "You're a saint thats alot of #{pet}"
+end
 
 
 
