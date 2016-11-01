5f691b60fb3ebf8513549a6a867a14af57b0197b
georgecode <georgeczurlanis@gmail.com>
2016-11-01 02:35:00 -0700

    add pseudocode to ArraysHashes5_2

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+#pseudocode
+#create an empty hash
+# gets.chop user input and add it to the hash as a key and value
+#set user input to the correct data type
+#print the hashes to screen
+#display info for user
+#prompt for update if they want to update update else print out final hash
+#close program and "Have a great day"
+
+
+
