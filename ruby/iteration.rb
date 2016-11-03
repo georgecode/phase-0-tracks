c4caf14282361c99a246ee2f011b07733d001d9a
georgecode <georgeczurlanis@gmail.com>
2016-11-03 14:59:57 -0700

    finish pairing 5_3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -30,4 +30,65 @@ sigil.each do |house, sigil|
   puts "#{sigil} sigil belongs to house #{house}"
 end
 
+#----------------
 
+
+
+letters = ["a","b","c","d","e","f"]
+
+numbers ={
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+p letters
+letters.reject!{|letter| letter>"d"}
+p letters
+
+p numbers
+numbers.reject!{|number_spell,num| number_spell.length == num}
+p numbers
+
+p letters
+letters.select!{|letter| letter>="b"}
+p letters
+
+p numbers
+numbers.select!{|number_spell,num| number_spell.length < num}
+p numbers
+
+
+
+letters = ["a","b","c","d","e","f"]
+
+numbers ={
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5
+}
+
+
+
+p letters
+letters.delete_if{|letter| letter>"d"}
+p letters
+
+
+p numbers
+numbers.delete_if{|number_spell,num| number_spell.length == num}
+p numbers
+
+puts "\n-----------------\n"
+
+p letters
+letters.keep_if{|letter| letter>="b"}
+p letters
+
+p numbers
+numbers.keep_if{|number_spell,num| number_spell.length < num}
+p numbers
