c6f024193c0edc86279b882e2440ea8998e45843
georgecode <georgeczurlanis@gmail.com>
2016-11-03 14:13:33 -0700

    Create array and hash

---
+++ ruby/iteration.rb
@@ -0,0 +1,10 @@
+game_of_thrones =["terious lanster", "john snow", "red lady", "serse lanaster", "stanus barathen", "the mountain"]
+
+sigil = {
+  lanster: "lion",
+  starkes: "dier wolf",
+  barathen: "staff",
+  grey_joy: "Kraken"
+}
+
+
