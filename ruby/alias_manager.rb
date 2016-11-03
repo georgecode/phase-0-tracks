107e52abab2f0e62eac2a4a62f8486197f7e9a20
georgecode <georgeczurlanis@gmail.com>
2016-11-02 22:24:56 -0700

    Add psudo code

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,13 @@
+# Swapping the first and last name.
+# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
+
+#psudo code
+#create method change_name
+#run method change_name
+#split name reverse name join name
+#change consonants first with .next unless vowel
+# make arry of vowles split name change vowels by advancing with the vowel array
+
+
+
+
