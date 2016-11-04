932c016446b96a4aca58db50d03efeedee088e05
georgecode <georgeczurlanis@gmail.com>
2016-11-04 14:07:57 -0700

    Add user interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,23 +1,17 @@
-# Swapping the first and last name.
-# Changing all of the vowels (a, e, i, o, or u) to the next vowel in 'aeiou', and all of the consonants (everything else besides the vowels) to the next consonant in the alphabet. So 'a' would become 'e', 'u' would become 'a', and 'd' would become 'f'.
-
 #psudo code
 #create method change_name
 #run method change_name
 #split name reverse name join name
-#change consonants first with .next unless vowel
-# make arry of vowles split name change vowels by advancing with the vowel array
+#change consonants first with conditional regx
+# loop through and case chand vowels
+#caps first letter
 
 def change_name(name)
   # swapes names
   name = name.split(' ').reverse.join(' ')
-  puts name
-  p name.split('')
 
   #change all consonant's
   name = name.downcase.split('').map do |l|
-    #.match(/[a-zA-Z]/)
-    # if l == " " || l == "a" || l == "e" || l== "i" || l=="o" ||l=="u"
     if l.match(/[a,e,i,o,u,\s]/)
       l = l
       #account for edge case
@@ -26,19 +20,13 @@ def change_name(name)
     else
       l = l.next
       #fixes consonant's that changed into vowels
-      #if l == "a" || l == "e" || l== "i" || l=="o" ||l=="u"
       if l.match(/[a,e,i,o,u]/)
         l.next
       else
         l = l
       end
-      #
     end
   end
-
-  p name
-
-
 #Loop through vowles
   for i in 0..name.length()-1
     case name[i]
@@ -52,20 +40,22 @@ def change_name(name)
         name[i] = "u"
       when "u"
         name[i] = "a"
-      # else
-      #   name[i] = name[i]
+
     end
   end
-p name
 name = name.join()
-p name
 name = name.split.map(&:capitalize).join(' ')
 p name
 end
+#test function
+# change_name("BOBz builder")
+# change_name("aaa eeeee BBB ZZ")
+# change_name("Felicia Torres")
 
-change_name("BOBz builder")
-change_name("aaa eeeee BBB ZZ")
-change_name("Felicia Torres")
-
-
-
+agent_name = ""
+#puts "Enter agent name"
+while agent_name !="quit"
+  puts "Enter agent name"
+  agent_name = gets.chomp
+  change_name(agent_name)
+end
