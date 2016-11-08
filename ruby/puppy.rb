e771bdd2e7aacad7085fc6d97daa3b19c2b65d0d
georgecode <georgeczurlanis@gmail.com>
2016-11-07 23:43:34 -0800

    Add 6.1 solutions

---
+++ ruby/puppy.rb
@@ -0,0 +1,32 @@
+# PUPPY-------------
+# Wagally_willy
+
+
+# CHARACTERISTICS---------
+# Color:  brown
+# Breed : lab
+# Months_old:5
+# Fav_food: bacon
+
+
+
+# BEHAVIOR-----------
+# Bark
+# Roll_over
+# Sit
+
+
+
+# The assignmet only told me to do whats above this line,
+#however the grading critera seems to require code so i
+#added it below
+
+class Puppy
+  def bark
+    p "bark, bark, bark"
+  end
+end
+
+Wagally_willy = Puppy.new()
+
+Wagally_willy.bark()
