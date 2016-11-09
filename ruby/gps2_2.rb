b668738400386eba7e0269ea1c3ee9d28bd62502
georgecode <georgeczurlanis@gmail.com>
2016-11-08 18:24:46 -0800

    Add psudo code

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:create an empty hash
+  # add a create list method(food_str)
+  # .split it
+  # set default to 1 test 0 too
+  #loop through list
+  # print the list to the console [can you use one of your other methods here?] print hash
+# output: [hash?]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps: add item food_str, and qty_num
+# output: updated hash
+
+# Method to remove an item from the list
+# input: food_str
+# steps: delete food_str
+# output: updated hash
+
+# Method to update the quantity of an item
+# input: food_str qty_num
+# steps: override qty
+# output:updated hash
+
+# Method to print a list and make it look pretty
+# input: hash
+# steps: loop make pretty
+# output: updated hash
