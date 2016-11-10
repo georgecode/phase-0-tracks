de6e8bb9a2c265b9099177ca5bb0ed6bbd402298
georgecode <georgeczurlanis@gmail.com>
2016-11-10 11:36:12 -0800

    Finish release one

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,37 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-end
+#   def speak(num)
+#     num.times {puts "Woof!"}
+#   end
 
+#   def roll_over
+#     puts "*rolls over*"
+#   end
+
+#   def dog_years(num)
+#     num * 7
+#   end
+
+#   def eat(num)
+#     puts "Eats #{num} of meals a day!"
+#   end
+
+#   def initializ
+#     puts "Initializing new puppy instance ..."
+#   end
+
+# end
+
+# #driver code
+# fluffy = Puppy.new
+
+# fluffy.fetch("bone")
+# fluffy.speak(3)
+# fluffy.roll_over
+# p fluffy.dog_years(10)
+# fluffy.eat(4)
