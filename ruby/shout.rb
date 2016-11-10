fedef858da98ee511897ff9dd5b27d0429a15f21
georgecode <georgeczurlanis@gmail.com>
2016-11-10 02:14:31 -0800

    Add flight.rb and updated shout.rb

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,16 +1,51 @@
 # to use require_relative 'shout'
 # to see methods Shout.singleton_methods
 
+# module Shout
+#   def self.yell_angrily(words)
+#   words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+
+# end
+
+# puts Shout.yell_angrily("Mucking work")
+# puts Shout.yell_happily("I can't belive that worked first try!!")
+
+
+
+# Up date shout(We're shouting I felt the need to .upcase)
 module Shout
-  def self.yell_angrily(words)
-  words + "!!!" + " :("
+
+  def yell_angrily(words)
+    words.upcase!
+    words + "!!!" + " :("
   end
 
-  def self.yell_happily(words)
+  def yell_happily(words)
+    words.upcase!
     words + "!!!" + " :)"
   end
 
 end
+#add two classes of things that might shout
+class The_Isley_Brothers
+  include Shout
+end
 
-puts Shout.yell_angrily("Mucking work")
-puts Shout.yell_happily("I can't belive that worked first try!!")
+class Donald_trump
+  include Shout
+end
+
+#driver code
+donald_trump = Donald_trump.new
+puts donald_trump.yell_angrily("You're Fired")
+
+the_isley_brothers = The_Isley_Brothers.new
+puts the_isley_brothers.yell_happily("Put your hands up and shout, come on now and shout")
+
+
+
