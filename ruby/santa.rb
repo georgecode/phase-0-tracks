19c8049ebe0b7ce07cd135ce30f76fa613e2ee1f
georgecode <georgeczurlanis@gmail.com>
2016-11-09 19:21:08 -0800

    Add diverse initializations with loop

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,12 +7,44 @@ class Santa
     p "That was a good #{cookie_type}!"
   end
 
-  def initialize
+  # def initialize()
+ def initialize(gender, ethnicity)
+    #These things with the @ are attributes
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
     p "Initializing Santa instance ..."
   end
 end
 
 # test code
-jingles = Santa.new()
-jingles.speak
-jingles.eat_milk_and_cookies("sugar cookie")
+# jingles = Santa.new()
+# jingles.speak
+# jingles.eat_milk_and_cookies("sugar cookie")
+
+
+#test code
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
+
+
+# Add some diverse initializations to your file as driver code
+genders =["Cisgender","Androgyne","Nonconforming", "Transpecies"]
+ethnicity =["Native Hawaiian", "Native American", "Aboriginal", "Gerald Broflovski(dolphin)"]
+
+genders.length.times do |i|
+  santas << Santa.new(genders[i], ethnicity[i])
+end
+
+# This is just here to see whats going on
+santas.each do |santa|
+  p santa
+  puts"\n-------------------------\n"
+end
