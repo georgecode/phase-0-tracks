1411bc979c628481f5a684366dd775797f3a2c85
georgecode <georgeczurlanis@gmail.com>
2016-11-10 12:09:10 -0800

    Finish 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -35,3 +35,39 @@
 # fluffy.roll_over
 # p fluffy.dog_years(10)
 # fluffy.eat(4)
+
+class Guitars
+
+  def sing(sound)
+    puts "Guitar goes #{sound}"
+  end
+
+  def price()
+    puts "This guitar is really expensive, you are now broke!"
+  end
+
+  def initialize
+    puts "Initializing new guitar instance ..."
+  end
+
+end
+
+fender = Guitars.new
+
+count = 0
+
+guitars = []
+while count < 51
+  guitars << Guitars.new
+  count += 1
+end
+
+
+# fender.sing("lalalala...")
+# fender.price
+
+guitars.each do |guitar|
+  puts "\n--------------"
+  guitar.sing("lalalala...")
+  guitar.price
+end
