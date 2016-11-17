3f8879c9e08ac0ec52cac7e55178f9764492e8bb
georgecode <georgeczurlanis@gmail.com>
2016-11-17 00:22:32 -0800

    Add sudo code

---
+++ js/explore.js
@@ -0,0 +1,11 @@
+/*I'll entertain you with the sudo code and the ugly solution asked for, but than i'll add a more eloquent way solution below
+1. create an empty function with a string
+2. convert the string into a split array
+3. loop through the letters of the array
+  3.1 reomve a letter off the back of the split array
+  3.2 add the removed letter to a new array
+4. join the new array
+5. output the new array*/
+
+
+
