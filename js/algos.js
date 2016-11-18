56dc08e5bd143b4d9e9ce86f3f65e29f903d2598
georgecode <georgeczurlanis@gmail.com>
2016-11-17 16:40:09 -0800

    Add 7.3 relase 1

--- js/algos.js
+++ js/algos.js
@@ -3,4 +3,27 @@
 2. find the word length
 3. make a conditional that stores the longest word
 5. return the word*/
-console.log("helllllllllo")
+/*-------What assignment asked for--------*/
+var words = ["long phrase","longest phrase","longer phrase"]
+
+function longestWordFinder(wordsArr){
+  var wordLength = 0;
+  var longestWord ="";
+  for(var i in wordsArr){
+    if(wordsArr[i].length >= wordLength){
+      wordLength = wordsArr[i].length;
+      longestWord = wordsArr[i];
+    }
+  }
+  return longestWord;
+}
+
+console.log(longestWordFinder(words));
+
+/*------Much better way------------*/
+function longestWordFinder2(wordsArr){
+  return wordsArr.sort(function (a, b) { return b.length - a.length; })[0];
+}
+
+console.log(longestWordFinder2(words));
+
