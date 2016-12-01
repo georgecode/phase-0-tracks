7b5e111ef8fbb849e18fbaccc0400c3dd7317a38
georgecode <georgeczurlanis@gmail.com>
2016-12-01 01:03:59 -0800

    jQuery 9.3 release2 add html, js and css

--- web_dev/jquery/jqueryProject/script.js
+++ web_dev/jquery/jqueryProject/script.js
@@ -1,5 +1,13 @@
 console.log("js works!!!")
 
-$( "body" ).click(function(){
-  console.log("jquery works");
+// $( "body" ).click(function(){
+//   console.log("jquery works");
+// });
+count = 0;
+$( "#sendInfo" ).click(function() {
+  var name = document.getElementById("name").value;
+  var blog = document.getElementById("blogInput").value;
+  count++;
+  $("article").append("<div class='blogPost'>post:"+count+"<br><em>User Name: "+name+"</em></br>"+blog+"</div>");
+
 });
