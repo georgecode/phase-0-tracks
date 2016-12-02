aef9a59b14b074e8d12d4d5becd2f0d14350895b
georgecode <georgeczurlanis@gmail.com>
2016-12-01 23:05:55 -0800

    Finish 9.4

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -1,3 +1,9 @@
+#gem install sinatra --no-rdoc --no-ri
+
+#gem install shotgun
+
+#localhost:8080/?name=george&age=29
+
 # require gems
 require 'sinatra'
 require 'sqlite3'
@@ -9,6 +15,7 @@ db.results_as_hash = true
 # add a query parameter
 # GET /
 get '/' do
+  "hello george!!!!!!"
   "#{params[:name]} is #{params[:age]} years old."
 end
 
@@ -44,3 +51,36 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+#1. A /contact route that displays an address (you can make up the address).
+get '/contact'  do
+  "123 sleepy rd. hillburg, CA 90777"
+end
+#2. A /great_job route that can take a person's name as a query parameter (not a route parameter) and say "Good job, [person's name]!". If the query parameter is not present, the route simply says "Good job!"
+get '/great_job/' do
+  if params[:person]
+    "Good job, #{params[:person]}!"
+  else
+    "Good job!"
+  end
+end
+
+# Optional bonus: Make a route that allows the user to search the database in some way -- maybe for students who have a certain first name, or some other attribute. If you like, you can simply modify the home page to take a query parameter, and filter the students displayed if a query parameter is present.
+# I know this is not what the bouns challenge asked for but it seemed more intresting to me so i explored it
+get '/vampires' do
+  vampires = db.execute("SELECT * FROM students")
+  vamps = ""
+
+  vampires.each do |vampires|
+    if vampires['age'] >= 100
+      vamps << "#{vampires['name']} is #{vampires['age']} and is likley a vampire!<br><br>"
+    end
+  end
+    vamps
+end
+
+# 1.Is Sinatra the only web app library in Ruby? What are some others?no. rails, lotus,Trailblazer,Cuba and a bunch of others.
+
+# 2.Are SQLite and the sqlite3 gem your only options for using a database with Sinatra? What are some others? no. mysql, mysql2, postgresql out of the box, you can use some others too with an adapter gem
+
+# 3.What is meant by the term web stack? all The technolgy you are useing, ie your db and frameworks.
